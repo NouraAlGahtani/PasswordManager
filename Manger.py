6d457c2f338fda946599b6e9a7ecb6c36e16ee4a
NOURA_ALGAHTANI <Nourahaniq@gmail.com>
2023-04-20 01:19:01 +0300

    yay finshed but Need bug fix

--- Manger.py
+++ Manger.py
@@ -6,29 +6,43 @@ class PassowrdManager:
   
   
   
+  
    #----------------- ADD FUNCTION--------------------------------------------- 
    
     #Make sure ur Attribtes are lowerCased!
-  def  add_Password(self, website,username,password):
-    hashed_password = ""
+def  add_Password(self, website,username,password):
+    hashed_password =""
     hashlib.sha256(password.encode()).hexdigest()
     if website in self.passwords:
         self.passwords[website][username] = hashed_password
     else:
-        self.password[website] = {username: hashed_password}
-        
-        
+        self.password[website] = {username: hashed_password}   
 # ----------------------RETRIVE FUNCTION---------------------------------------
-  def get_Password(self, website, username):
+def get_Password(self, website, username):
     if website in self.passwords and username in self.passwords[website]:
         return self.passwords[website][username]
     else:
         return None 
-    print("UserName invaild")
+   
 #---------------------------DELETE FUNCTION-------------------------------------
 def delete_password(self,website,username):
     if website in self.passwords and username in self.passwords[website]:
-        del self.
+        del self.passwords[website][username]
+        if not bool(self.passwords[website]):
+            del self.passwords[website]
+#----------------------------------CHANGE FUNCTION---------------------------------------------
     
-
+def change_password(self, website, username, new_password):
+    hashed_new_password = ""
+    hashlib.sha256(new_password.encode()).hexdigest()
+    if website in self.passwords and username in self.passwords[website]:
+        self.passwords[website][username] = hashed_new_password
         
+
+password_manager = PassowrdManager
+password_manager(add_Password("", "gfdsak", "loren", "fds3"))
+
+
+    
+    
+      
